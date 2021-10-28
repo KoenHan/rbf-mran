38bd2efaf5de250822699ef67e4f2a245e02340e
KoenHan <sliver1998324@gmail.com>
2021-10-28 10:25:45 +0900

    学習データ書き換え前

--- study/quad2_wxp_new/data/make_train_from_all.py
+++ study/quad2_wxp_new/data/make_train_from_all.py
@@ -1,6 +1,41 @@
+
+
+class QuadWxp :
+    def __init__(self) :
+        U_G = 73.0
+        U_DIFF_LIM = 61.0
+        MAX_RPS    = U_G + U_DIFF_LIM
+
+        self.dt = 0.01
+        self.A_OF_GRAVITY = 9.80
+        self.VAR_SIZE = 13
+        self.I_XX = 0.007
+        self.I_YY = 0.007
+        self.I_ZZ = 0.012
+        self.ROTOR_DISTANCE = 0.34
+        self.MASS_OF_MACHINE = 0.716
+        self.MAX_RPS_POW = MAX_RPS**2
+        self.MAX_THRUST = self.MASS_OF_MACHINE*self.A_OF_GRAVITY*self.MAX_RPS_POW/(4*U_G*U_G)
+        self.TORQUE_RATE = 0.25/MAX_RPS/MAX_RPS
+        # self.RADIAN = 3.1415
+        # self.TWICE_RADIAN = 2*self.RADIAN
+
+        # self.u = [0 for _ in range(4)]
+        # self.var_p = [0 for _ in range(self.VAR_SIZE)]
+        # self.var_and_z_i = [0 for _ in range(self.VAR_SIZE)]
+        # self.var_and_z_i[0] = 1.0 # クオータニオンだから
+
+        # self.GAIN = 1e4
+
+    def wxp_f(self, ww, uuuu) :
+        wxp = 0.5*(2.0*(self.I_YY-self.I_ZZ)*ww+self.ROTOR_DISTANCE*self.MAX_THRUST*uuuu/self.MAX_RPS_POW)/self.I_XX
+        return wxp
+
 if __name__ == "__main__" :
     GAIN = 1e4
     datas = []
+
+    quadwxp = QuadWxp()
     with open("quat_rate_sysin.txt", "r") as f:
         datas = [s.strip().split() for s in f.readlines()]
 
@@ -12,7 +47,7 @@ if __name__ == "__main__" :
     with open("train.txt", "w") as f:
         f.write('2\n')
         f.write('1\n') # wxp
-        f.write('2\n') # 下のwとu
+        f.write('2\n') # 下のwwとuuuu
         for idx, data in enumerate(datas) :
             if idx == 0:
                 pre_wx = float(data[4])
@@ -20,14 +55,15 @@ if __name__ == "__main__" :
             wx = float(data[4])
             wy = float(data[5])
             wz = float(data[6])
-            wxp = wx - pre_wx
-            w = wy*wz
+            org_wxp = wx - pre_wx
+            ww = wy*wz
             # rps_cw1 = GAIN*float(data[-4])
             # rps_cw2 = GAIN*float(data[-3])
             rps_ccw1 = GAIN*float(data[-2])
             rps_ccw2 = GAIN*float(data[-1])
-            # u = rps_ccw1*abs(rps_ccw1) - rps_ccw2*abs(rps_ccw2) # 論文では下になってるのになぜかこれになってた
-            u = rps_ccw1**2 - rps_ccw2**2
+            uuuu = rps_ccw1*abs(rps_ccw1) - rps_ccw2*abs(rps_ccw2) # 論文では下になってるのになぜかこれになってた
+            # uuuu = rps_ccw1**2 - rps_ccw2**2
+            wxp = quadwxp.wxp_f(ww, uuuu)
             tmp = "\t".join([str(wxp), str(pre_wx), str(u)])
             f.write(tmp+"\n")
             pre_wx = wx
