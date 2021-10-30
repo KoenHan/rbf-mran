dcd0e7039167f0ac92f3d2c67bbce0d1cc7cb060
KoenHan <sliver1998324@gmail.com>
2021-10-30 20:15:48 +0900

    pc切り替え

--- study/quad2_wxp/data/cut_data.py
+++ study/quad2_wxp/data/cut_data.py
@@ -3,13 +3,13 @@
 '''
 
 if __name__ == "__main__" :
-    start = 2000000
-    target_line_num = 1000000
+    start = 0
+    target_line_num = 10000
     start += 3 # 先頭の3行は必ずいるので
     end = start + target_line_num + 3
     cnt = 0
     infile = "train_def.txt"
-    outfile = "test.txt"
+    outfile = "train.txt"
     with open(infile, "r") as ifile, open(outfile, "w") as ofile :
         while cnt < end :
             line = ifile.readline()
