b78e7c0a3e5bbde147895cfa08dc4dc656c73b5e
KoenHan <sliver1998324@gmail.com>
2021-10-30 15:16:39 +0900

    いらないファイル削除

--- study/ros_test_pos2/data/make_train_from_all.py
+++
@@ -1,9 +0,0 @@
-if __name__ == "__main__" :
-    datas = []
-    with open("quat_rate_sysin.txt", "r") as f:
-        datas = [s.strip().split() for s in f.readlines()]
-
-    with open("train.txt", "w") as f:
-        for i, data in enumerate(datas) :
-            tmp = "\t".join(data[-7:])
-            f.write(tmp+"\n")
