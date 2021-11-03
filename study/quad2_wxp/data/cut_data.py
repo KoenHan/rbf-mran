0e38b7618c037a1a1e6bc117f44188759387a504
KoenHan <sliver1998324@gmail.com>
2021-11-03 12:44:01 +0900

    wxpとxppのネットワークサイズが小さいやつが使えるようにした

--- study/quad2_wxp/data/cut_data.py
+++ study/quad2_wxp/data/cut_data.py
@@ -1,24 +1,18 @@
-import argparse
-
 '''
 データファイルを指定行数に削減する
 '''
 
 if __name__ == "__main__" :
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-o', '--output', type=str, required=True)
-    parser.add_argument('-cs', '--cut_start', type=int, required=True)
-    parser.add_argument('-cl', '--cut_len', type=int, required=True)
-    args = parser.parse_args()
-
-    start = args.cut_start
+    start = 0
+    target_line_num = 1000000
     start += 3 # 先頭の3行は必ずいるので
-    target_line_num = args.cut_len
-    end = start + target_line_num
+    end = start + target_line_num + 3
     cnt = 0
-    with open("train_def.txt", "r") as infile, open(args.output, "w") as outfile :
+    infile = "train_def.txt"
+    outfile = "train.txt"
+    with open(infile, "r") as ifile, open(outfile, "w") as ofile :
         while cnt < end :
-            line = infile.readline()
+            line = ifile.readline()
             if cnt < 3 or cnt >= start :
-                outfile.write(line)
+                ofile.write(line)
             cnt += 1
--- study/quad2_xpp/data/cut_data.py
+++ study/quad2_xpp/data/cut_data.py
@@ -1,22 +1,14 @@
-import argparse
-
 '''
 データファイルを指定行数に削減する
 '''
 
 if __name__ == "__main__" :
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-o', '--output', type=str, required=True)
-    parser.add_argument('-cs', '--cut_start', type=int, required=True)
-    parser.add_argument('-cl', '--cut_len', type=int, required=True)
-    args = parser.parse_args()
-
-    start = args.cut_start
+    start = 4500000
     start += 3 # 先頭の3行は必ずいるので
-    target_line_num = args.cut_len
+    target_line_num = 1000000
     end = start + target_line_num
     cnt = 0
-    with open("train_def.txt", "r") as infile, open(args.output, "w") as outfile :
+    with open("train_def.txt", "r") as infile, open("test.txt", "w") as outfile :
         while cnt < end :
             line = infile.readline()
             if cnt < 3 or cnt >= start :
